6b697a2b397f1bc1de1dcfd9beedffe21a7c41df
kavinda-ravishan <kvnravishan@outlook.com>
2021-06-25 11:26:52 +0530

    Search Bin Rec.

--- mycodeschool_binary_search/Application.cpp
+++ mycodeschool_binary_search/Application.cpp
@@ -13,9 +13,7 @@ int Search(T* arr, unsigned int size, T value) {
 template <typename T>
 int SearchBin(T* arr, unsigned int size, T value) {
 
-	int start = 0;
-	int end = size - 1;
-	int mid;
+	int start = 0, end = size - 1, mid;
 
 	while (start <= end)
 	{
@@ -29,13 +27,36 @@ int SearchBin(T* arr, unsigned int size, T value) {
 	return -1;
 }
 
+
+template <typename T>
+int SearchBin(T* arr, T value, int start, int end) {
+
+	if (start > end) return -1;
+	
+	int mid = (start + end) / 2;
+
+	if (arr[mid] == value) return mid;
+	else if (value < arr[mid]) return SearchBin(arr, value, start, mid - 1);
+	else return SearchBin(arr, value, mid + 1, end);
+
+}
+
+template <typename T>
+int SearchBinRec(T* arr, unsigned int size, T value) {
+
+	int start = 0, end = size - 1;
+
+	return SearchBin<T>(arr, value, start, end);
+}
+
 int main(int argc, char** args) {
 
 	const unsigned int size = 5;
-	int x[7] = { 2,14,32,51,89 };
+	int x[] = { 2,14,32,51,89 };
 
 	std::cout << Search<int>(x, size, 32) << std::endl;
 	std::cout << SearchBin<int>(x, size, 32) << std::endl;
+	std::cout << SearchBinRec<int>(x, size, 32) << std::endl;
 
 	return 0;
 }
